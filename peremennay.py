aa194609144a5455f73ad1844d4cd694ac97fd2c
VernerKari <karinavern04@gmail.com>
2024-11-30 15:58:02 +0300

    пр. Переменная

---
+++ peremennay.py
@@ -0,0 +1,5 @@
+completed_HW=12
+time_spent=1.5
+name_coures="Python"
+time_for_one_task=time_spent/completed_HW
+print('Курс:',name_coures,"всего задач:",completed_HW,"затрачено часов:",time_spent,"среднее время выполнения",time_for_one_task, "часа")
